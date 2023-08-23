ee2d9b0d5afaefd2b6d0bf5545260e4a6d3103ac
PNS212624\Hansol <yrlee00@hansol.com>
2023-08-23 18:58:44 +0900

    8월 23일 실습

---
+++ src/main/java/com/example/cyworld/CyworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.cyworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cyworld/CyworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cyworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

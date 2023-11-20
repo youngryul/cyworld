8952f3386d27bff021f8ef5cac4074620d479a27
PNS212624\Hansol <yrlee00@hansol.com>
2023-11-20 17:27:40 +0900

    11월 20일 4차 실습 완료

---
+++ src/main/resources/static/game.js
@@ -0,0 +1,19 @@
+function startWord () {
+    let myword = document.getElementById("myword").value
+    let word = document.getElementById("word").innerText
+
+    let lastword = word[word.length - 1]
+    let firstword = myword[0]
+
+    if(lastword === firstword){
+        //정답일때
+        document.getElementById("result").innerText = '정답입니다!'
+        document.getElementById("word").innerText = myword
+        document.getElementById("myword").value = ""
+    }
+    else {
+        //오답일때
+        document.getElementById("result").innerText = "땡!"
+        document.getElementById("myword").value = ""
+    }
+}
